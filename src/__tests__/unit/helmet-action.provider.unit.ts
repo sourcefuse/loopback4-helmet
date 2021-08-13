ad1844ca27b933d56bc6fae4a40e91e12e583343
Yesha  Mavani <yesha.mavani@sourcefuse.com>
2021-08-13 17:40:40 +0530

    test(provider): test cases and coverage added
    GH-13

---
+++ src/__tests__/unit/helmet-action.provider.unit.ts
@@ -0,0 +1,28 @@
+import {IHelmetConfiguration} from 'helmet';
+import {HelmetActionProvider} from '../../providers';
+import {expect, stubExpressContext} from '@loopback/testlab';
+
+describe('Helmet Service', () => {
+  describe('Helmet Provider', () => {
+    it('returns a promise after success', async () => {
+      const config: IHelmetConfiguration = {
+        referrerPolicy: {
+          policy: 'same-origin',
+        },
+        contentSecurityPolicy: {
+          directives: {
+            frameSrc: ["'self'"],
+          },
+        },
+      };
+      const context = stubExpressContext({
+        url: '/',
+        method: 'GET',
+        payload: {key: 'value'},
+      });
+      const handler = new HelmetActionProvider(config).value();
+      const result = handler(context.request, context.response);
+      await expect(result).to.be.fulfilled();
+    });
+  });
+});
